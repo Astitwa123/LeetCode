c7d92614ecde3abe68a34dc6f5de21fa7d723108
Astitwa123 <111843677+Astitwa123@users.noreply.github.com>
2022-09-13 14:19:44 +0530

    Time: 24 ms (6.11%), Space: 13.6 MB (63.75%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,69 @@
 
 class Solution {
 public:
+    int first(vector<int>& nums , int target)
+    {
+        int s=0;
+        int ans=-1;
+        int e=nums.size()-1;
+        
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target)
+            {
+                ans=mid;
+                e=mid-1;
+            }
+            else if(nums[mid]<target)
+            {
+                s=mid+1;
+                
+            }
+            else
+            {
+                e=mid-1;
+            }
+        }
+        return ans;
+        
+    }
+       int last(vector<int>& nums , int target)
+    {
+        int s=0;
+        int ans=-1;
+        int e=nums.size()-1;
+        
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target)
+            {
+                ans=mid;
+                s=mid+1;
+            }
+            else if(nums[mid]<target)
+            {
+                s=mid+1;
+                
+            }
+            else
+            {
+                e=mid-1;
+            }
+        }
+        return ans;
+        
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> v;
-        int flag=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(target==nums[i])
-            {
-                int first=i;
-                v.push_back(first);
-                flag=1;
-                break;
-            }
-           
-        }
-        if(flag==0)
-        {
-            v.push_back(-1);
-        }
-        flag=0;
-            for(int i=nums.size()-1;i>=0;i--)
-        {
-            if(target==nums[i])
-            {
-                int last=i;
-                v.push_back(last);
-                flag=1;
-                break;
-            }
-           
-        }
-        if(flag==0)
-        {
-            v.push_back(-1);
-        }
+        vector<int> v(2,-1);
+        int x=first(nums,target);
+        if(x==-1)return v;
+        
+        int y=last(nums,target);
+        if(y==-1)return v;
+        v[0]=x;
+        v[1]=y;
         return v;
     }
 };
