7115465f4e4a220fbbb57ae0fec0ce6c1309ae23
Astitwa123 <111843677+Astitwa123@users.noreply.github.com>
2022-09-23 13:47:00 +0530

    Time: 16 ms (34.69%), Space: 14.9 MB (42.56%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -3,69 +3,52 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+
+ *     ListNode(int x) : val(x), next(NULL) {}
+
  * };
  */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        struct ListNode* tmp= new ListNode(0), *head= new ListNode(0);
-        if(l1==NULL && l2==NULL)
-            return NULL;
+        ListNode *finalhead=NULL;
         if(l1==NULL)return l2;
         if(l2==NULL)return l1;
-        int flag=0;
-        while(l1!=NULL && l2!=NULL)
-              {
-                 if(l1->val<l2->val)
-                 {
-                     if(flag==0)
-                     {      
-                        head=l1;
-                        tmp=head;
-                     }
-                 
-                  else
-                  {
-                      tmp->next=l1;
-                      tmp=tmp->next;
-                  }
-                     l1=l1->next;
-                 }
-                 else{ 
-                     if(flag==0)
-                     {
-                         head=l2;
-                         tmp=head;
-                         
-                     }
-                    else
-                    {
-                        tmp->next=l2;
-                        tmp=tmp->next;
-                    }
-                     l2=l2->next;
-                    
-                  
-                  
-                    }
-                  flag=1;
-              }
-        while(l1!=NULL)
-              {
-                  tmp->next=l1;
-                  tmp=tmp->next;
-                  l1=l1->next;
-              } 
-               while(l2!=NULL)
-              {
-                  tmp->next=l2;
-                  tmp=tmp->next;
-                  l2=l2->next;
-              } 
-        return head;
+        if(l1==NULL && l2==NULL)return NULL;
+        
+        if(l1->val <l2->val)
+        {
+            finalhead=l1;
+            l1=l1->next;
+        }
+        else{
+            finalhead=l2;
+            l2=l2->next;
+        }
+        ListNode *p=finalhead;
+        while(l1 && l2)
+        {
+            if(l1->val <l2->val)
+            {
+                p->next=l1;
+                l1=l1->next;
+            }
+            else
+            {
+                p->next=l2;
+                l2=l2->next;
+            }
+            p=p->next;
+        }
+        if(l1)
+        {
+            p->next=l1;
+        }
+        else
+        {
+            p->next=l2;
+        }
+        return finalhead;
         }
               
 };
