6acc85c83852ce10de63b48ab1117d636b7592be
Astitwa123 <111843677+Astitwa123@users.noreply.github.com>
2022-10-04 15:02:44 +0530

    Time: 46 ms (66.39%), Space: 25.7 MB (29.14%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,10 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         vector<int>v=nums;
-        if(k>n)
-        {
-            k%=n;
-        }
+   
         for(int i=0;i<n;i++)
         {
             nums[(i+k)%n]=v[i];
