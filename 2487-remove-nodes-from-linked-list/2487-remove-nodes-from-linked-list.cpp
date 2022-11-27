52aaff0c4953c0d7f1265ebcde7323caf42937eb
Astitwa123 <111843677+Astitwa123@users.noreply.github.com>
2022-11-27 23:56:13 +0530

    Time: 1096 ms (12.50%), Space: 163.6 MB (12.50%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -6,28 +6,29 @@
  *     ListNode() : val(0), next(NULL) {}
  *  
  */
-
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        if(head->next == NULL) return head;
-        ListNode* ptr = head;
-        deque<ListNode*> s;
-        while(ptr != NULL){
-            if(!s.empty()) {
-                while(!s.empty() && s.back()->val < ptr->val) s.pop_back(); 
+        if(head->next==NULL)return head;
+        ListNode* ptr =head;
+        deque<ListNode*>s;
+        while(ptr!=NULL)
+        {
+            if(!s.empty())
+            {
+                while(!s.empty() && s.back()->val < ptr->val)s.pop_back();
             }
             s.push_back(ptr);
-            ptr = ptr->next;
+            ptr=ptr->next;
         }
-        if(s.empty() ) return NULL;
-        head = s.front(); ptr = s.front(); s.pop_front();
+        if(s.empty())return NULL;
+        ptr=s.front(),head=s.front(),s.pop_front();
         while(!s.empty()){
-            ptr->next = s.front();
-            ptr = ptr->next;
+            ptr->next=s.front();
+            ptr=ptr->next;
             s.pop_front();
         }
-        ptr->next = NULL;
+        ptr->next=NULL;
         return head;
     }
 };
